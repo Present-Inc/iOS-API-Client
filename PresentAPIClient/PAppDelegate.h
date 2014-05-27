dccda41a6dc1c88e6f0deaf06a7ff37da9143790
justinmakaila <justinmakaila@gmail.com>
2014-05-27 15:37:03 -0400

    Initial Commit

---
+++ PresentAPIClient/PAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  PAppDelegate.h
+//  PresentAPIClient
+//
+//  Created by Justin Makaila on 5/27/14.
+//  Copyright (c) 2014 Present, Inc. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface PAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ PresentAPIClient/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  PresentAPIClient
+//
+//  Created by Justin Makaila on 5/27/14.
+//  Copyright (c) 2014 Present, Inc. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "PAppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([PAppDelegate class]));
+    }
+}
