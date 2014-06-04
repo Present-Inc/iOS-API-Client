4a34f6934dd928009e755b05dbbe7c79a60c88fb
justinmakaila <justinmakaila@gmail.com>
2014-06-04 17:28:25 -0400

    Added PComment+ResourceMethods.*

---
+++ PresentAPIClient/Models/PComment+ResourceMethods.h
@@ -0,0 +1,13 @@
+//
+//  PComment+ResourceMethods.h
+//  PresentAPIClient
+//
+//  Created by Justin Makaila on 6/4/14.
+//  Copyright (c) 2014 Present, Inc. All rights reserved.
+//
+
+#import "PComment.h"
+
+@interface PComment (ResourceMethods)
+
+@end
---
+++ PresentAPIClient/Models/PComment+ResourceMethods.m
@@ -0,0 +1,13 @@
+//
+//  PComment+ResourceMethods.m
+//  PresentAPIClient
+//
+//  Created by Justin Makaila on 6/4/14.
+//  Copyright (c) 2014 Present, Inc. All rights reserved.
+//
+
+#import "PComment+ResourceMethods.h"
+
+@implementation PComment (ResourceMethods)
+
+@end
